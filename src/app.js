4179abfe3754d045df0bdfa82b34ccc1851adf33
Priyasaha7 <148933801+Priyasaha7@users.noreply.github.com>
2026-01-13 19:27:19 +0530

    exploring get, post and delete apis

--- src/app.js
+++ src/app.js
@@ -10,14 +10,33 @@ app.use("/test", (req, res) => {
     res.send("Hello from the test server")
 });
 
-app.use("/", (req, res) => {
-    res.send("Hello from the home page")
-}); 
+// app.use("/", (req, res) => {
+//     res.send("Hello from the home page")
+// }); 
 // keep "/" at the bootom always beacuse
 // app.use("/") matches every route
 // /, /test, /hello → all start with /
 // So Express stops at the first match and never reaches /test or /hello
 // That’s why no matter what you click, you always get home page response.
+
+
+//This will only handle get call api to /user
+app.get("/user", (req, res) => {
+    res.send("Get the details of the user")
+});
+
+//This will only handle post call api to /user
+app.post("/user", (req, res) => {
+    // saving data to DB
+    res.send("Added user to the database")
+});
+
+//This will only handle delete call api to /user
+app.delete("/user", (req, res) => {
+    // deleting data from DB
+    res.send("Deleted user from the database")
+}); 
+
 
 app.listen(7777, () => {
     console.log("Server is successfully listening on port 7777");
