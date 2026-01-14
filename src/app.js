8cbcd0dbaae5b8a0f488394bb08b7365b91adc8c
Priyasaha7 <148933801+Priyasaha7@users.noreply.github.com>
2026-01-14 17:49:14 +0530

    writing-auth-middleware-for-admin-and-user

--- src/app.js
+++ src/app.js
@@ -1,16 +1,38 @@
 const express = require("express");
 
+const { adminAuth, userAuth } = require("./middleWare/auth")
+
 const app = express();
+
+app.use("/admin", adminAuth);
+app.use("/user", userAuth);
+
+app.use("/user", (req, res) => {
+    res.send("User data send")
+});
+
+app.use("/admin/getAllData", (req, res) => {
+    res.send("All Data Sent")
+});
+
+app.use("/admin/delete", (req, res) => {
+    res.send("Deleted a user")
+});
+
+
+
+
+
 
 // app.use("/user", (req, res) => {
 //     console.log(req.query);
 //     res.send({ firstname: "Priya", lastname: "Saha"});
 // });
 
-app.get("/user/:userID/:name/:password", (req, res) => {
-    console.log(req.params);
-    res.send({ firstname: "Priya", lastname: "Saha"});
-});
+// app.get("/user/:userID/:name/:password", (req, res) => {
+//     console.log(req.params);
+//     res.send({ firstname: "Priya", lastname: "Saha"});
+// });
 
 app.listen(7777, () => {
     console.log("Server is successfully listening on port 7777");
