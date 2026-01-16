80fc3cbd40308f450fef01787513bdf6477cb9ea
Priyasaha7 <148933801+Priyasaha7@users.noreply.github.com>
2026-01-16 11:37:53 +0530

    feat: add CRUD APIs for user management with MongoDB
    - Implemented POST /signup to create users using request body data
    - Added GET /user endpoint to fetch a user by emailID
    - Added GET /feed endpoint to retrieve all users from database
    - Implemented DELETE /user to remove a user by ID
    - Implemented PATCH /user to update user details by ID
    - Added express.json() middleware for JSON request parsing
    - Integrated proper error handling for all database operations

--- src/app.js
+++ src/app.js
@@ -7,9 +7,13 @@ const app = express()
 // converting the json data to js so that js understand
 app.use(express.json());
 
+
+// saving our data to databse from postman 
+// creating a new instance of the user model
+// req.body -> app.use(express.json()); will convert and dump the data into body so that we can use and made it dynamic
+
+
 app.post("/signup", async (req, res) => {
-    // creating a new instance of the user model
-    // req.body -> app.use(express.json()); will convert and dump the data into body so that we can use and made it dynamic
     const user = new User(req.body)
     console.log(req.body);
     
@@ -22,6 +26,100 @@ app.post("/signup", async (req, res) => {
     }
     
 });
+
+
+
+//getting our data from the databse 
+// Get user by emailID
+
+// For finding one user
+app.get("/user", async(req, res) => {
+    try{
+        const userEmail = req.body.emailID;    
+        const user = await User.findOne({emailID:userEmail});        
+        if(user.length === 0){
+            res.status(404).send("User not found");
+        }
+        else{
+            res.send(user);
+        }
+    }catch(err){
+        res.status(404).send("Something went wrong");
+    }
+})
+
+
+// For Finding Users
+
+// app.get("/user", async(req, res) => {
+//     try{
+//         const userEmail = req.body.emailID;    
+//         const user = await User.find({emailID:userEmail});
+
+//         if(user.length === 0){
+//             res.status(404).send("User not found");
+//         }
+//         else{
+//             res.send(user);
+//         }
+//     }catch(err){
+//         res.status(404).send("Something went wrong");
+//     }
+// })
+
+
+
+
+
+
+
+
+
+
+
+
+//Feed api - GET /feed -> get all the users from the databse
+app.get("/feed", async (req, res) => {
+    try{
+        const users = await User.find({});
+        res.send(users);
+    }
+    catch(err){
+        res.status(404).send("Something went wrong");
+    }
+})
+
+
+// Delete api 
+app.delete("/user", async(req, res) => {
+    const userID = req.body.userID;
+    try{
+        //const user = await User.findByIdAndDelete({userID});
+        const user = await User.findByIdAndDelete({ _id: userID});
+        res.send("User deleted successfully");
+    }catch{
+        res.status(400).send("Something went wrong");
+    }
+});
+
+
+
+// Update data
+app.patch("/user", async(req, res) => {
+    const userID = req.body.userID;
+    const data = req.body;
+    try{
+        const user = await User.findByIdAndUpdate({ _id: userID}, data,{ 
+            returnDocument: "before",
+         });
+        console.log(user);
+        
+        res.send("User updated successfully");
+    }catch{
+        res.status(400).send("Something went wrong");
+    }
+});
+
 
 
 connectDB()
