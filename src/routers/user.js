79211059ffbb876042cf07bfc4a4ab775363eed2
Priyasaha7 <148933801+Priyasaha7@users.noreply.github.com>
2026-01-21 00:21:27 +0530

    feat: implement pagination with skip and limit in Mongoose

--- src/routers/user.js
+++ src/routers/user.js
@@ -58,6 +58,11 @@ userRouter.get("/feed", userAuth, async (req, res) => {
     try{
         const loggedInUser = req.user;
 
+        const page = parseInt(req.query.page) || 1;
+        let limit = parseInt(req.query.limit) || 10;
+        limit = limit>50 ? 50 : limit;
+        const skip = (page-1)*limit;
+
         const connectionRequest = await ConnectionRequest.find({
             $or:[
                 {fromUserId: loggedInUser._id},
@@ -78,7 +83,10 @@ userRouter.get("/feed", userAuth, async (req, res) => {
                 { _id: {$nin: Array.from(hideUsersFromFeed)}},
                 { _id: {$ne: loggedInUser._id}},
             ]
-        }).select(USER_PROFILE_FIELDS);
+        })
+        .select(USER_PROFILE_FIELDS)
+        .skip(skip)
+        .limit(limit);
 
         res.json({ data: users });
 
