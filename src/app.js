60a52594c8d2b752f6181946799f44d1882b84e8
Priyasaha7 <148933801+Priyasaha7@users.noreply.github.com>
2026-01-24 15:39:07 +0530

    feat: add CORS support and update dependencies in package.json and package-lock.json

--- src/app.js
+++ src/app.js
@@ -7,7 +7,18 @@ const profileRouter = require("./routers/profile");
 const requestRouter = require("./routers/request");
 const userRouter = require("./routers/user");
 
-const app = express()
+const cors = require("cors");
+
+// creating an express app
+const app = express();
+
+// enabling cors for all the routes
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  }),
+);
 
 // converting the json data to js so that js understand
 app.use(express.json());
@@ -16,23 +27,19 @@ app.use(cookieParser());
 app.use("/", authRouter);
 app.use("/", profileRouter);
 app.use("/", requestRouter);
-app.use("/",userRouter);
+app.use("/", userRouter);
 
-// saving our data to databse from postman 
+// saving our data to databse from postman
 // creating a new instance of the user model
 // req.body -> app.use(express.json()); will convert and dump the data into body so that we can use and made it dynamic
 
-
-
 connectDB()
-    .then(() => {
-        console.log("Database connection established...");
-        app.listen(7777, () => {
-            console.log("Server is successfully listening on port 7777"); 
-        });
-    })
-    .catch((err) => {
-        console.error("Database cannot be connected!!"); 
-    })
-
-
+  .then(() => {
+    console.log("Database connection established...");
+    app.listen(7777, () => {
+      console.log("Server is successfully listening on port 7777");
+    });
+  })
+  .catch((err) => {
+    console.error("Database cannot be connected!!");
+  });
