dc46ab25665f84859ac7aedfe75e205df3486fe5
Priyasaha7 <148933801+Priyasaha7@users.noreply.github.com>
2026-01-26 00:31:12 +0530

    update auth.js and user.js

--- src/middleWare/auth.js
+++ src/middleWare/auth.js
@@ -2,28 +2,26 @@ const jwt = require("jsonwebtoken");
 const User = require("../models/user");
 
 const userAuth = async (req, res, next) => {
-    try{
-        const token = req.cookies.token;
-        if(!token){
-            throw new Error("Invalid Token");
-        };
-
-        const decodedObj = jwt.verify( token , "DEV@TINDER#2004");
-        const { _id } = decodedObj;
-
-        const user = await User.findById({ _id });
-
-        if(!user){
-            throw new Error("User not found");
-        };
-
-        req.user = user;
-        next();
+  try {
+    const token = req.cookies.token;
+    if (!token) {
+      return res.status(401).send("Please login to access this resource");
     }
 
-    catch(err){
-        res.status(401).send("Unauthorized: No token provided");
+    const decodedObj = jwt.verify(token, "DEV@TINDER#2004");
+    const { _id } = decodedObj;
+
+    const user = await User.findById({ _id });
+
+    if (!user) {
+      throw new Error("User not found");
     }
+
+    req.user = user;
+    next();
+  } catch (err) {
+    res.status(401).send("Unauthorized: No token provided");
+  }
 };
 
-module.exports = {userAuth};
+module.exports = { userAuth };
--- src/routers/user.js
+++ src/routers/user.js
@@ -1,99 +1,89 @@
 const express = require("express");
 const userRouter = express.Router();
-const { userAuth }= require("../middleWare/auth");
+const { userAuth } = require("../middleWare/auth");
 const ConnectionRequest = require("../models/connectionRequest");
-const User = require("../models/user")
+const User = require("../models/user");
 
-const USER_PROFILE_FIELDS = "firstName lastName about age gender skills";
+const USER_PROFILE_FIELDS =
+  "firstName lastName about age gender skills photoUrl";
 
-userRouter.get("/user/requests/received", userAuth, async (req, res)=>{
-    try{
-        const loggedInUser = req.user;
+userRouter.get("/user/requests/received", userAuth, async (req, res) => {
+  try {
+    const loggedInUser = req.user;
 
-        const connectionRequest = await ConnectionRequest.find({
-            toUserId: loggedInUser._id,
-            status: "interested",
-        }).populate("fromUserId", USER_PROFILE_FIELDS);
+    const connectionRequest = await ConnectionRequest.find({
+      toUserId: loggedInUser._id,
+      status: "interested",
+    }).populate("fromUserId", USER_PROFILE_FIELDS);
 
-        res.status(200).json({
-            message: "Connection requests retrieved successfully",
-            data: connectionRequest,
-        });
-
-    }
-    catch(err){
-        res.status(400).send("ERROR" + err.message);
-    }
+    res.status(200).json({
+      message: "Connection requests retrieved successfully",
+      data: connectionRequest,
+    });
+  } catch (err) {
+    res.status(400).send("ERROR" + err.message);
+  }
 });
 
 userRouter.get("/user/connections", userAuth, async (req, res) => {
-    try{
+  try {
+    const loggedInUser = req.user;
 
-        const loggedInUser = req.user;
+    const connectionRequest = await ConnectionRequest.find({
+      $or: [
+        { fromUserId: loggedInUser._id, status: "accepted" },
+        { toUserId: loggedInUser._id, status: "accepted" },
+      ],
+    })
+      //.populate("fromUserId toUserId", USER_PROFILE_FIELDS);
+      .populate("fromUserId", USER_PROFILE_FIELDS)
+      .populate("toUserId", USER_PROFILE_FIELDS);
 
-        const connectionRequest = await ConnectionRequest.find({
-            $or:[
-                { fromUserId: loggedInUser._id, status: "accepted"},
-                { toUserId: loggedInUser._id, status: "accepted"},
-            ]
-        })
-        //.populate("fromUserId toUserId", USER_PROFILE_FIELDS);
-        .populate("fromUserId", USER_PROFILE_FIELDS)
-        .populate("toUserId", USER_PROFILE_FIELDS);
-
-
-        const data = connectionRequest.map((k) => {
-            if(k.fromUserId._id.toString() === loggedInUser._id.toString()){
-                return k.toUserId;
-            }
-            return k.fromUserId;
-        })
-         res.status(200).json({data});
-    }catch(err){
-        res.status(400).send("ERROR" + err.message);
-    }
+    const data = connectionRequest.map((k) => {
+      if (k.fromUserId._id.toString() === loggedInUser._id.toString()) {
+        return k.toUserId;
+      }
+      return k.fromUserId;
+    });
+    res.status(200).json({ data });
+  } catch (err) {
+    res.status(400).send("ERROR" + err.message);
+  }
 });
 
 userRouter.get("/feed", userAuth, async (req, res) => {
-    try{
-        const loggedInUser = req.user;
+  try {
+    const loggedInUser = req.user;
 
-        const page = parseInt(req.query.page) || 1;
-        let limit = parseInt(req.query.limit) || 10;
-        limit = limit>50 ? 50 : limit;
-        const skip = (page-1)*limit;
+    const page = parseInt(req.query.page) || 1;
+    let limit = parseInt(req.query.limit) || 10;
+    limit = limit > 50 ? 50 : limit;
+    const skip = (page - 1) * limit;
 
-        const connectionRequest = await ConnectionRequest.find({
-            $or:[
-                {fromUserId: loggedInUser._id},
-                {toUserId: loggedInUser._id},
-            ]
-        }).select("fromUserId toUserId");
+    const connectionRequest = await ConnectionRequest.find({
+      $or: [{ fromUserId: loggedInUser._id }, { toUserId: loggedInUser._id }],
+    }).select("fromUserId toUserId");
 
+    const hideUsersFromFeed = new Set();
+    connectionRequest.forEach((req) => {
+      hideUsersFromFeed.add(req.fromUserId.toString());
+      hideUsersFromFeed.add(req.toUserId.toString());
+    });
 
-        const hideUsersFromFeed = new Set();
-        connectionRequest.forEach((req) => {
-            hideUsersFromFeed.add(req.fromUserId.toString());
-            hideUsersFromFeed.add(req.toUserId.toString());
-        });
+    const users = await User.find({
+      $and: [
+        { _id: { $nin: Array.from(hideUsersFromFeed) } },
+        { _id: { $ne: loggedInUser._id } },
+      ],
+    })
+      .select(USER_PROFILE_FIELDS)
+      .skip(skip)
+      .limit(limit);
 
-
-        const users = await User.find({
-            $and:[
-                { _id: {$nin: Array.from(hideUsersFromFeed)}},
-                { _id: {$ne: loggedInUser._id}},
-            ]
-        })
-        .select(USER_PROFILE_FIELDS)
-        .skip(skip)
-        .limit(limit);
-
-        res.json({ data: users });
-
-    }
-    catch(err){
-        res.status(400).send("ERROR" + err.message);
-    }
+    res.json({ data: users });
+  } catch (err) {
+    res.status(400).send("ERROR" + err.message);
+  }
 });
 
 module.exports = userRouter;
